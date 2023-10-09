da77072e828473ee9ee14468a587f374f90d9e3b
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2023-10-09 09:27:20 +0200

    Add search params to x2t.wasm to prevent caching
    https://github.com/cryptpad/cryptpad/issues/1278

--- pre-js.js
+++ pre-js.js
@@ -1,2 +1,16 @@
 Module.noInitialRun = true;
 Module.noExitRuntime = true;
+(function() {
+	let suffix;
+	if (typeof document != 'undefined') {
+		const myScript = document.currentScript;
+		const mySrc = myScript.getAttribute('src');
+		suffix = new URL(mySrc).search;
+	} else {
+		suffix = '';
+	}
+
+	Module.locateFile = function(path, prefix) {
+		return prefix + path + suffix;
+	};
+})();
